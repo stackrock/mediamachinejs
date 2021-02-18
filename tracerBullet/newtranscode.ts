ecc977fda694dfb822e4fe23654e51d7d9b41126
Sergio Rafael Gianazza <sgianazza@gmail.com>
2021-02-17 22:00:59 -0300

    added unhandle rejection handler on all tracerbullets

--- tracerBullet/newtranscode.ts
+++ tracerBullet/newtranscode.ts
@@ -1,10 +1,14 @@
+process.on("unhandledRejection", (error) => {
+  throw error;
+});
+
 /*
  * Tracer Bullet for a transcode job.
  * We use this job internally at MediaMachine for two reasons:
  *  1) To keep the SDK in sync with API
  *  2) To Test our API is running as expected
  */
-require('dotenv').config();
+require("dotenv").config();
 import { MediaMachine } from "../src";
 
 import { sleep } from "./utils";
@@ -21,15 +25,27 @@ async function main() {
   const mediaMachine = new MediaMachine(MEDIAMACHINE_API_KEY);
 
   try {
-
-    const job = await mediaMachine.transcodeToMp4({
-      encoder: "h264",
-      height: 300,
-      width: 150,
-      watermark: mediaMachine.textWatermark("mediamachine.io"),
-    })
-    .fromS3(AWS_REGION, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, BUCKET, INPUT_KEY)
-    .toS3(AWS_REGION, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, BUCKET, OUTPUT_KEY)
+    const job = await mediaMachine
+      .transcodeToMp4({
+        encoder: "h264",
+        height: 300,
+        width: 150,
+        watermark: mediaMachine.textWatermark("mediamachine.io"),
+      })
+      .fromS3(
+        AWS_REGION,
+        AWS_ACCESS_KEY_ID,
+        AWS_SECRET_ACCESS_KEY,
+        BUCKET,
+        INPUT_KEY
+      )
+      .toS3(
+        AWS_REGION,
+        AWS_ACCESS_KEY_ID,
+        AWS_SECRET_ACCESS_KEY,
+        BUCKET,
+        OUTPUT_KEY
+      );
     let status = await job.status();
 
     while (status === "queued") {
